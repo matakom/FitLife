1651e153d0aeacdbbdca00fce5d989abe6c4331c
matakom <mata.komarek@gmail.com>
2024-04-26 16:46:56 +0200

    Testing health connect connection. Needs to be tested more

--- frontend/fit_life/lib/home.dart
+++ frontend/fit_life/lib/home.dart
@@ -4,6 +4,7 @@ import 'package:pedometer/pedometer.dart';
 import 'package:permission_handler/permission_handler.dart' as permission;
 import 'steps.dart' as steps_counter;
 import 'package:flutter/services.dart';
+import 'package:flutter_health_connect/flutter_health_connect.dart';
 
 const methodChannel = MethodChannel('kotlinChannel');
 
@@ -16,6 +17,37 @@ Future<void> getUsageStats() async {
   on PlatformException catch (e) {
     print("Failed to get usage stats: '${e.message}'.");
   }
+}
+
+Future<void> healthTest() async{
+
+  //android/app/build.gradle dependencies finish
+
+  bool readOnly = true;
+
+  List<HealthConnectDataType> types = [
+    HealthConnectDataType.Steps,
+  ];
+
+  var startTime = DateTime.now().subtract(const Duration(days: 4));
+  var endTime = DateTime.now();
+
+  print('isApiSupported: ${await HealthConnectFactory.isApiSupported()}');
+  print('isAvailable: ${await HealthConnectFactory.isAvailable()}');
+  //await HealthConnectFactory.installHealthConnect();
+  //HealthConnectFactory.openHealthConnectSettings();
+  print('hasPermissions: ${await HealthConnectFactory.hasPermissions(types, readOnly: readOnly,)}');
+  await HealthConnectFactory.requestPermissions(types, readOnly: readOnly,);
+  print('hasPermissions: ${await HealthConnectFactory.hasPermissions(types, readOnly: readOnly,)}');
+  try{
+    var results = await HealthConnectFactory.getRecord(type: types.first ,startTime: startTime,endTime: endTime,);
+    print('Results: $results');
+  } on Exception catch (e){
+    print("Exception: $e");
+  }
+
+
+
 }
 
 class Home extends StatefulWidget {
@@ -60,6 +92,10 @@ class _HomeState extends State<Home> {
     }
 
     getUsageStats();
+
+    healthTest();
+
+
 
   }
 
